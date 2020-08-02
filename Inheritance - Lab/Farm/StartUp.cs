d46f6dc90148b759a885c6a42f1b67432d3d3945
Milen Ivanov <58393766+milen92sl@users.noreply.github.com>
2020-08-03 00:27:53 +0300

    Add files via upload

---
+++ Inheritance - Lab/Farm/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Animal();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new Animal();
+            cat.Eat();
+            cat.Meow();
+
+        }
+    }
+}
